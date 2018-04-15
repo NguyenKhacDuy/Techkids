3d46df3f3c39def264018216ddb179337174a1ad
NguyenKhacDuy <duythachson@gmail.com>
2018-04-15 20:54:35 +0700

    abc

--- Back-End/api/images/model.js
+++ Back-End/api/images/model.js
@@ -2,22 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const imageModel = new Schema({
-    imangeUrl: {type: String, required: true},
-    title: {type: String, required: true},
-    description: {type:String,default: ''},
-    createdBy: {type: String, required: true}, //khi co user: type: Schema.types.ObjectId
-    view: {type: Number, default: 0},
-    like: {type:Number, default: 0},
-    active: {type: boolean, default: true},
-    comment: {type: [commentModel], default: []}
+    imangeUrl: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, default: '' },
+    createdBy: { type: String, required: true }, //khi co user: type: Schema.types.ObjectId
+    view: { type: Number, default: 0 },
+    like: { type: Number, default: 0 },
+    active: { type: boolean, default: true },
+    comment: { type: [commentModel], default: [] }
 },
-    {timestamps: true}
+    { timestamps: true }
 );
 
 const commentModel = new Schema({
-    createdBy: {type: String}
+    createdBy: { type: String },
+    content: { type: String, required: true}
 },
-    {timestamps: true}
+    { timestamps: true }
 )
 
 module.exports = mongoose.model("images", imageModel);
